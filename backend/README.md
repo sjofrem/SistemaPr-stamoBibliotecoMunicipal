a49b0f9b1c11ca12295ee18088f5e0c700f0bac8
Alonso Zamorano <azamorano@cangrejoideas.com>
2023-06-07 21:50:44 -0400

    Initial steps torwards backend

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+# Backend
+
+Inspirado en [Build CRUD APIs Using Apollo Server(Graphql), MongoDB and Node.Js](https://reflectoring.io/tutorial-graphql-apollo-server-nodejs-mongodb/).
+
+## Para instalar dependencias
+
+```
+npm install
+```
+
+También es necesario instalar [MongoDB](https://www.mongodb.com/docs/manual/installation/).
+
+## Para correr
+
+```
+npm run dev
+```
---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const mongoose = require("mongoose");
+const { resolvers } = require("./resolvers.js");
+const { typeDefs } = require("./models/typeDefs.js");
+
+const mongoURL = "mongodb://localhost:27017/student-register";
+
+// Connect to database
+mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(`Database connected!`);
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+
+// Serve application
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+    listen: { port: 4000 },
+}).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
